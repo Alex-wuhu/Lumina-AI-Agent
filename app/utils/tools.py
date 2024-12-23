80ee97665430d80bd8e2f0c872e330457dbdf05c
DVDguzhou <1537006648@qq.com>
2024-12-24 00:05:48 +0800

    合约测试

--- app/utils/tools.py
+++ app/utils/tools.py
@@ -74,18 +74,83 @@ class BuyCryptos:
         # 创建账户
         self.private_key = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
         self.account = Account.from_key(self.private_key)
+        print(f"使用账户地址: {self.account.address}")
 
     def check_balances(self):
         """检查用户余额"""
         eth_balance = self.agent_contract.functions.getETHBalance().call({'from': self.account.address})
-        print(f"当前ETH余额: {self.w3.from_wei(eth_balance, 'ether')} ETH")
+        print(f"合约中的ETH余额: {self.w3.from_wei(eth_balance, 'ether')} ETH")
+        
+        # 检查账户的 BNB 余额
+        account_balance = self.w3.eth.get_balance(self.account.address)
+        print(f"账户BNB余额: {self.w3.from_wei(account_balance, 'ether')} BNB")
 
         if self.trade_type != 'ETH_TO_TOKEN':
             token_balance = self.agent_contract.functions.getTokenBalance(self.input_token).call({'from': self.account.address})
-            print(f"当前代币余额: {self.w3.from_wei(token_balance, 'ether')} Token")
+            print(f"合约中的代币余额: {self.w3.from_wei(token_balance, 'ether')} Token")
+            
+            # 检查账户的代币余额
+            erc20_abi = [
+                {
+                    "constant": True,
+                    "inputs": [{"name": "_owner", "type": "address"}],
+                    "name": "balanceOf",
+                    "outputs": [{"name": "balance", "type": "uint256"}],
+                    "type": "function"
+                }
+            ]
+            token_contract = self.w3.eth.contract(address=self.input_token, abi=erc20_abi)
+            account_token_balance = token_contract.functions.balanceOf(self.account.address).call()
+            print(f"账户代币余额: {self.w3.from_wei(account_token_balance, 'ether')} Token")
+            
             return token_balance >= self.w3.to_wei(self.amount, 'ether')
 
         return eth_balance >= self.w3.to_wei(self.amount, 'ether')
+
+    def approve_token(self, token_address, amount):
+        """授权代币"""
+        try:
+            amount_in_wei = self.w3.to_wei(amount, 'ether')
+            
+            # 创建 ERC20 合约实例
+            erc20_abi = [
+                {
+                    "constant": False,
+                    "inputs": [
+                        {"name": "_spender", "type": "address"},
+                        {"name": "_value", "type": "uint256"}
+                    ],
+                    "name": "approve",
+                    "outputs": [{"name": "", "type": "bool"}],
+                    "type": "function"
+                }
+            ]
+            token_contract = self.w3.eth.contract(address=token_address, abi=erc20_abi)
+            
+            # 授权代币
+            nonce = self.w3.eth.get_transaction_count(self.account.address)
+            approve_tx = token_contract.functions.approve(
+                self.AGENT_ADDRESS,
+                amount_in_wei
+            ).build_transaction({
+                'from': self.account.address,
+                'gas': 100000,
+                'gasPrice': self.w3.eth.gas_price,
+                'nonce': nonce,
+                'chainId': 97
+            })
+            
+            # 签名并发送授权交易
+            signed_approve_tx = self.w3.eth.account.sign_transaction(approve_tx, self.private_key)
+            approve_tx_hash = self.w3.eth.send_raw_transaction(signed_approve_tx.rawTransaction)
+            print(f"授权交易已发送，等待确认... Hash: {approve_tx_hash.hex()}")
+            self.w3.eth.wait_for_transaction_receipt(approve_tx_hash)
+            print("代币授权成功")
+            return True
+            
+        except Exception as e:
+            print(f"Error approving tokens: {e}")
+            return False
 
     def execute_trade(self):
         """执行交易"""
@@ -93,6 +158,11 @@ class BuyCryptos:
             # 检查余额
             if not self.check_balances():
                 return "余额不足"
+
+            # 如果是代币交易，需要先授权
+            if self.trade_type in ['TOKEN_TO_ETH', 'TOKEN_TO_TOKEN']:
+                if not self.approve_token(self.input_token, self.amount):
+                    return "代币授权失败"
 
             # 准备通用参数
             amount_in = self.w3.to_wei(self.amount, 'ether')
